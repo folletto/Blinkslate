91da0e248c9d58650b48a45dc20504759db4f2d2
Folletto <folletto@gmail.com>
2010-07-28 01:32:20 +0100

    Added "read all" and "comment" call to actions.

--- posts/default.list.php
+++ posts/default.list.php
@@ -5,9 +5,11 @@
       <?php if ($post->post_excerpt) { ?>
       <p class="excerpt">
          <?php echo get_the_excerpt(); ?>
+         <div class="post-call-to-action-excerpt"><a href="<?php the_permalink() ?>">&rarr; read all</a></div>
       </p>
       <?php } else { ?>
         <?php the_content(); ?>
+        <div class="post-call-to-action-full"><a href="<?php the_permalink() ?>">&rarr; comment</a></div>
       <?php } ?>
       
       <!--
