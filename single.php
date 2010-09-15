2a562ee604261eb867e4e50212d03af263583d25
Folletto <folletto@gmail.com>
2010-09-16 00:37:21 +0100

    Optionalized related posts.

--- single.php
+++ single.php
@@ -6,19 +6,22 @@
 			  @include wpp::get_category_matching_partial('posts', 'php');
   		endwhile; ?>
   		
+  		<?php $related = wpp::related_posts(
+  		  3,
+  		  'article', // category filter
+  		  '<h4><a href="%post_permalink%">%post_title%</a></h4>',
+  		  null, // dateformat
+  		  0, // post_id
+  		  'nil'
+  		); 
+  		if ($related != 'nil') { ?>
   		<div id="related">
   		  <div class="tray">
   		    <span class="black">Related posts</span>
   		  </div>
-    		<?php echo wpp::related_posts(
-    		  3,
-    		  'article', // category filter
-    		  '<h4><a href="%post_permalink%">%post_title%</a></h4>',
-    		  null, // dateformat
-    		  0, // post_id
-    		  '<span class="smallstatus">Nothing yet.</span>'
-    		); ?>
+    		<?php echo $related; ?>
   	  </div>
+	    <?php } ?>
   	  
   		<?php comments_template(); ?>
   		
