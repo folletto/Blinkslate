688c5be20ceb98f9df001a7ef56e1d74e8b0fd60
Folletto <folletto@gmail.com>
2010-10-11 12:47:57 +0100

    Fix: typo fixed "stripos" to exclude local domains.

--- cfg/meta-footer.php
+++ cfg/meta-footer.php
@@ -1,3 +1,3 @@
-<?php if (!wpp::is_admin() && stripoi($_SERVER['SERVER_NAME'], '.local') !== false) { // excluding administrators from the stats ?>
+<?php if (!wpp::is_admin() && stripos($_SERVER['SERVER_NAME'], '.local') !== false) { // excluding administrators from the stats ?>
 <!-- Add here Google Analytics and other stuffs -->
 <?php } ?>
